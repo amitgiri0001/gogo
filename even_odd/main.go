02c701cbb8c184de9771bf06c10ff167c4c85efd
Amit Giri <amit.giri0001@gmail.com>
2022-02-15 06:35:09 +0000

    even odd assigment

---
+++ even_odd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, n := range numbers {
+		nType := "odd"
+
+		if n%2 == 0 {
+			nType = "even"
+		}
+
+		fmt.Println(n, "is", nType)
+	}
+}
