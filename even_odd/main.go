8356841821dc2f403e0d95b7a3a23de65f29c83a
Amit Giri <amit.giri0001@gmail.com>
2022-02-15 06:38:27 +0000

    even odd assigment

--- even_odd/main.go
+++ even_odd/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
 
 	for _, n := range numbers {
 		nType := "odd"
