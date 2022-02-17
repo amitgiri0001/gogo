04b573f824aee3ced3e29bc1859467cab9a7b797
Amit Giri <amit.giri0001@gmail.com>
2022-02-17 10:28:40 +0000

    struct - assignment by value

--- struct/main.go
+++ struct/main.go
@@ -8,14 +8,32 @@ type Person struct {
 }
 
 func main() {
+	/**
+	 * Sequential assignment
+	 */
 	newPerson := Person{"Amit", "Giri"}
 	fmt.Printf("%+v \n", newPerson)
 
+	/**
+	 * JS Object like assignment
+	 */
 	newPerson = Person{firstName: "Amit_1", lastName: "Giri_1"}
 	fmt.Printf("%+v \n", newPerson)
 
+	/**
+	 * instance values
+	 */
 	newPerson.firstName = "Amit_2"
 	newPerson.lastName = "Giri_2"
+	fmt.Printf("%+v \n", newPerson)
+
+	/**
+	 * Proves that GO is a pass by "value" language not by "ref"
+	 */
+	person2 := newPerson
+	fmt.Printf("%+v \n", person2)
+	person2.firstName = "Not Amit"
+	fmt.Printf("%+v \n", person2)
 	fmt.Printf("%+v \n", newPerson)
 
 }
